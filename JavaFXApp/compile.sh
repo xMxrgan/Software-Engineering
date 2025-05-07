7240eb1942fae27577200e87e50084e40c7ec9df
AuroraLeardini <167792580+AuroraLeardini@users.noreply.github.com>
2025-05-07 18:08:44 +0200

    modifica compile.sh

--- JavaFXApp/compile.sh
+++ JavaFXApp/compile.sh
@@ -9,4 +9,5 @@ javac \
   --module-path "$JAVAFX_LIB" \
   --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.web \
   -d bin \
-  src/com/Main.java
+  src/com/Main.java \
+  src/com/view/ViewNavigator.java
