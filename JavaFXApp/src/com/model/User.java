267c4c72cf7f779ad3d9fffcb23d4b69ef8d3b5c
AuroraLeardini <167792580+AuroraLeardini@users.noreply.github.com>
2025-05-07 18:59:41 +0200

    cartella css

--- JavaFXApp/src/com/model/User.java
+++ JavaFXApp/src/com/model/User.java
@@ -4,6 +4,9 @@ public class User {
     private int ID;
     private String nome;
     private String cognome;
+    private String email;
+    private String password;
+    private Boolean isAdmin;
 
     public User() {
     }
